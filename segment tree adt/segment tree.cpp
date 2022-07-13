fef66fd6fb362bb0838637b850981d16602e6bce
Chirag Sachdeva <109076194+Chirag-Sachdeva30@users.noreply.github.com>
2022-07-13 09:19:54 +0530

    Update segment tree.cpp

--- segment tree adt/segment tree.cpp
+++ segment tree adt/segment tree.cpp
@@ -1,21 +1,26 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 
-void maketree(int arr[],int ss,int se,int a[],int i)
+class segment_tree
 {
-    if(ss==se)
-    {
-        a[i]=arr[ss];
-        return ;
-    }
-    int mid=(ss+se)/2;
-    maketree(arr,ss,mid,a,2*i+1);
-    maketree(arr,mid+1,se,a,2*i+2);
-    a[i]=a[2*i+1]+a[2*i+2];
-}
+    private:
+        vector<int> a;
+        int* arr;
+        int n;
+        void maketree(int ss,int se,int i)
+        {
+            if(ss==se)
+            {
+                a[i]=arr[ss];
+                return ;
+            }
+            int mid=(ss+se)/2;
+            maketree(ss,mid,2*i+1);
+            maketree(mid+1,se,2*i+2);
+            a[i]=a[2*i+1]+a[2*i+2];
+        }
 
-int rangequery(int a[],int rs,int re,int ss,int se,int i)
+    int rangequery(int rs,int re,int ss,int se,int i)
 {
     int mid=(ss+se)/2;
     if(rs>se || re<ss)
@@ -23,22 +28,48 @@ int rangequery(int a[],int rs,int re,int ss,int se,int i)
     else if(rs<=ss && re>=se)
         return a[i];
 
-    else return rangequery(a,rs,re,ss,mid,2*i+1)+rangequery(a,rs,re,mid+1,se,2*i+2);
+    else return rangequery(rs,re,ss,mid,2*i+1)+rangequery(rs,re,mid+1,se,2*i+2);
 }
 
-void update(int a[],int arr[],int idx,int newval,int ss,int se,int i)
+void updatequerry(int idx,int ss,int se,int i)
 {
 
     int mid=(ss+se)/2;
-    if(idx>se || idx<ss)
-    return;
-    else if(idx>=ss && idx<=se)
-        a[i]=a[i]-arr[idx]+newval;
-        if(ss==se)
+      if(ss==se)
+        {
+            a[i]=arr[ss];
             return;
-        update(a,arr,idx,newval,ss,mid,2*i+1);
-         update(a,arr,idx,newval,mid+1,se,2*i+2);
+        }
+    if(idx<=mid)
+    updatequerry(idx,ss,mid,2*i+1);
+    else
+    updatequerry(idx,mid+1,se,2*i+2);
+    a[i]=a[2*i+1]+a[2*i+2];
 }
+
+    public:
+    segment_tree(int arr[],int n)
+    {
+        a.resize(4*n);
+        this->arr=arr;
+        this->n=n;
+        maketree(0,n-1,0);
+    }
+
+    int range(int l,int r)
+    {
+        return rangequery(l,r,0,n-1,0);
+    }
+    void update(int i,int newval)
+    {
+        arr[i]=newval;
+        updatequerry(i,0,n-1,0);
+    }
+};
+
+
+
+
 
 int main()
 {
@@ -47,9 +78,8 @@ int main()
     int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
-    int a[4*n];
-    maketree(arr,0,n-1,a,0);
-    update(a,arr,0,100,0,n-1,0);
-    cout<<rangequery(a,0,3,0,n-1,0);
+    segment_tree st1(arr,n);
+    st1.update(0,100);
+    cout<<st1.range(0,3);
     return 0;
 }
